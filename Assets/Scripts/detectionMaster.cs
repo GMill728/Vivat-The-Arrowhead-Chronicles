f04fa753886d113d28bfdd23b6348ad9fdbabd0d
GMill728 <gmill728@live.kutztown.edu>
2025-04-02 15:49:58 -0400

    added documentation to detection master

--- Assets/Scripts/detectionMaster.cs
+++ Assets/Scripts/detectionMaster.cs
@@ -9,6 +9,7 @@ using JetBrains.Annotations;
 public class Detection : MonoBehaviour
 {
     public static bool Game_Over = false;
+    //below are where to put detection images
     public Image image_0;
     public Image image_1;
     public Image image_2;
@@ -16,58 +17,66 @@ public class Detection : MonoBehaviour
     public Image image_4;
     public Image image_5;
 
-    int dStat = 0;
-    int maxD = 24;
-    int minD = 0;
+    int dStat = 0;//this is the initial value of detection status
+    int maxD = 24;//max detection status
+    int minD = 0;//minimum detection status
    
     void Start()
     {
-          updateDetection();
+          updateDetection();//on start check detection once (used for function declaration)
     }
     void Update() 
     {
-        if (Input.GetKeyDown(KeyCode.Equals))
+        if (Input.GetKeyDown(KeyCode.Equals))//check if plus was pressed (equals key)
         {
-            dStat++; //could pass 3 variables to deteciton to eliminate this line
-            updateDetection();
-            addFrame();
+            dStat++; //add one to detection status
+            updateDetection();//run update detection script
+            addFrame();//add one to the detection UI frames
         }
         else if (Input.GetKeyDown(KeyCode.Minus))
         {
-            dStat--;
-            updateDetection();
-            subtractFrame();
+            dStat--;//minus one to detection status
+            updateDetection();//run update detection script
+            subtractFrame();//minus one to the detection UI frames
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha0))
+        else if (Input.GetKeyDown(KeyCode.Alpha0))//if the user hits 0
         {
-            dStat = 0;
-            updateDetection();
-            zeroFrame();
+            dStat = 0;//zero out detection status
+            updateDetection();//update detection
+            zeroFrame();//zero out UI frame
         }
 
     }
-    void updateDetection() {
-        if (dStat > maxD){
-            dStat = maxD;
-            Game_Over = true;
+    void updateDetection() {//update detection function
+        if (dStat > maxD){//if the status is greater than max, 
+            dStat = maxD;//set status to max
+            Game_Over = true;//set global game over bool to true
         }
-        else if (dStat < minD){
+        else if (dStat < minD){//if status is less than 0, set it to 0
             dStat = minD;
         }
-        else if (dStat == 0) {
-            }
     }
-    void PlusAlpha(Image b){
-        var tempColor = b.color;
-         tempColor.a += 0.25f;
-         b.color = tempColor;
+    void PlusAlpha(Image b){//function to add alpha (transparency to input image)
+        var tempColor = b.color;//creates a temp var of all color channels (you can't just edit em directly)
+         tempColor.a += 0.25f;//adds .25 (out of max 1) to alpha since we are doing 4 frames per image
+         b.color = tempColor;//assigns this new alpha to the image
     }
-    void MinusAlpha(Image b){
+    void MinusAlpha(Image b){//does the same thing as above but minus
         var tempColor = b.color;
          tempColor.a -= 0.25f;
          b.color = tempColor;
     }
-    void addFrame(){
+    void ZeroAlpha(Image b){//does the same thing as above but zeros out the alpha
+        var tempColor = b.color;
+         tempColor.a = 0;
+         b.color = tempColor;
+    }
+    void addFrame(){//adding frames function description below
+    /*
+        going 0->23 (24 frames), every 4 frames is a new image with the same operations.
+        to do this we just assign which image it is and call the PlusAlpha funciton
+        to add alpha to the image, giving the illusion of more frames and a fade in
+    */
         if (dStat <= 3){
          PlusAlpha(image_0);
         }
@@ -87,7 +96,7 @@ public class Detection : MonoBehaviour
           PlusAlpha(image_5);
         }
     }
-    void subtractFrame(){
+    void subtractFrame(){//this does the same as above but subtraction
         if (dStat <= 3){
          MinusAlpha(image_0);
         }
@@ -107,12 +116,12 @@ public class Detection : MonoBehaviour
           MinusAlpha(image_5);
         }
     }
-    void zeroFrame(){
-        MinusAlpha(image_0);
-        MinusAlpha(image_1);
-        MinusAlpha(image_2);
-        MinusAlpha(image_3);
-        MinusAlpha(image_4);
-        MinusAlpha(image_5);
+    void zeroFrame(){ //this just calls the ZeroAlpha() function which sets the alpha of an image to 0 (transparent)
+        ZeroAlpha(image_0);
+        ZeroAlpha(image_1);
+        ZeroAlpha(image_2);
+        ZeroAlpha(image_3);
+        ZeroAlpha(image_4);
+        ZeroAlpha(image_5);
     }
 }
