7295f99591122f0ba4eb8714003652d5f3f3df1e
Luke Daugherty <ldaug031@live.kutztown.edu>
2025-05-11 19:38:16 -0400

    Fixed epilogue bug
    Fixed "not delivering message" option for player causing null exception for epilogue

--- Assets/Scripts/showEpilogue.cs
+++ Assets/Scripts/showEpilogue.cs
@@ -20,7 +20,7 @@ public class showEpilogue : MonoBehaviour
         {
             DialogueManager.Instance.linkActorVar = "EpilDialObj";  //update DialogueManager's temp variables for circumstances requiring these strings
             DialogueManager.Instance.linkNodeIdVar = "5";
-            DialogueManager.Instance.SpeakToNewActor("EpilDialObj", "5");
+            DialogueManager.Instance.SpeakToNewActor("EpilDialObj", "4");
         }
     }
 
